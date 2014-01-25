16cd1db628e90ab9924b3926414081edb5ef4c81
Adrian Cooney <cooney.adrian@gmail.com>
2014-01-25 02:12:41 +0000

    Documentation

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const QUOTE = "\"";
-const ARRAY_LEFT = "[";
-const ARRAY_RIGHT = "]";
-const OBJECT_LEFT = "{";
-const OBJECT_RIGHT = "}";
-const COLON = ":";
-const COMMA = ",";
-
+/**
+ * A 100% faster implementation of JSON.stringify because
+ * it strips the frills and confetti.
+ * @param  {Object} object An object to encode.
+ * @return {String}        JSON encoded string.
+ */
 JSON.stringify2 = function(object) {
+	// Stringify arrays by looping over their values
 	if(Array.isArray(object)) {
 		var output = "[";
 		for(var i = 0, cache = object.length; i < cache; i++) output += (i ? COMMA : "") + JSON.stringify2(object[i]);
 		return output + "]";
 	}
 
+	// Recur over objects
 	if(object !== null && typeof object === "object") {
 		var output = "{", i = 0;
 		for(var key in object) {
@@ -24,10 +24,13 @@ JSON.stringify2 = function(object) {
 		return output + "}";
 	}
 
+	// Return null without quotes
 	if(object === null) return null;
 
+	// Return boolean and numbers without quotes
 	var type = typeof object;
 	if(type === "number" || type === "boolean") return object;
 
+	// Return strings, with quotes
 	return "\"" + object + "\"";
 };
